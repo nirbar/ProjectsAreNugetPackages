7041a363567c718ae3a7c8989fad5d9f471c59b2
nir.bar <nir.bar@panel-sw.com>
2017-12-21 12:19:32 +0200

    MSBuild task to resolve build order. Instead of the usual build order (resolve target project dependencies and build them first), it resolves dependants build order. That is, projects that require the latest build of target project.

--- ProjectRefToPackage/PackagesConfig.cs
+++ ProjectRefToPackage/PackagesConfig.cs
@@ -16,6 +16,82 @@ namespace ProjectRefToPackage
         public PackagesConfig(string file)
         {
             packagesConfigFile_ = file;
+            Id = Path.GetFileName(Path.GetDirectoryName(file));
+        }
+
+        public string Id { get; private set; }
+
+        private List<string> dependencies_;
+        public IEnumerable<string> Dependencies
+        {
+            get
+            {
+                if (dependencies_ != null)
+                {
+                    return dependencies_;
+                }
+
+                dependencies_ = new List<string>();
+                if ((xmlDoc_ == null) && !File.Exists(packagesConfigFile_))
+                {
+                    return dependencies_;
+                }
+
+                if (xmlDoc_ == null)
+                {
+                    xmlDoc_ = new XmlDocument();
+                    xmlDoc_.Load(packagesConfigFile_);
+                }
+
+                foreach (XmlNode n in xmlDoc_.DocumentElement.ChildNodes)
+                {
+                    if ((n is XmlElement) && (n.LocalName.Equals("package")))
+                    {
+                        string dep = n.Attributes["id"]?.Value;
+                        if (!string.IsNullOrEmpty(dep))
+                        {
+                            dependencies_.Add(dep);
+                        }
+                    }
+                }
+
+                return dependencies_;
+            }
+        }
+
+        private List<string> impliedDependencies_;
+        /// <summary>
+        /// Recursively detect all dependencies
+        /// </summary>
+        /// <param name="allProjPackages"></param>
+        /// <returns></returns>
+        public List<string> ResolveRecursiveDependencies(Dictionary<string, PackagesConfig> allProjPackages)
+        {
+            if (impliedDependencies_ != null)
+            {
+                return impliedDependencies_;
+            }
+
+            impliedDependencies_ = new List<string>(Dependencies);
+
+            foreach (string dep in Dependencies)
+            {
+                // Dependency is not a local project or has no dependencies of its own?
+                if (!allProjPackages.ContainsKey(dep))
+                {
+                    continue;
+                }
+
+                List<string> depDep = allProjPackages[dep].ResolveRecursiveDependencies(allProjPackages);
+                if (depDep.Contains(Id))
+                {
+                    throw new ArgumentOutOfRangeException($"Cyclic dependency discovered {Id} <--> {dep}");
+                }
+
+                impliedDependencies_.AddRange(depDep);
+            }
+            
+            return impliedDependencies_;
         }
 
         public void EnsureExists()
@@ -43,7 +119,7 @@ namespace ProjectRefToPackage
 
             foreach (XmlNode n in xmlDoc_.DocumentElement.ChildNodes)
             {
-                if ((n is XmlElement) && (n.LocalName.Equals("package")) && (n.Attributes["id"]?.Name?.Equals(packageId, StringComparison.OrdinalIgnoreCase) == true))
+                if ((n is XmlElement) && (n.LocalName.Equals("package")) && (n.Attributes["id"]?.Value?.Equals(packageId, StringComparison.OrdinalIgnoreCase) == true))
                 {
                     return;
                 }
@@ -53,6 +129,8 @@ namespace ProjectRefToPackage
             xElem.SetAttribute("id", packageId);
             xElem.SetAttribute("version", "0.0.0.0");
             xmlDoc_.DocumentElement.AppendChild(xElem);
+            dependencies_ = null; // Invalidate dependency list.
+            impliedDependencies_ = null;
         }
 
         public void Save()
---
+++ ProjectRefToPackage/ResolveDependants.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using System.IO;
+
+namespace ProjectRefToPackage
+{
+    /// <summary>
+    /// Order projects by build order, assuming each project's packages.config contain dependencies on other projects.
+    /// </summary>
+    public class ResolveDependants : Task, ICancelableTask
+    {
+        private bool cancel_ = false;
+
+        public override bool Execute()
+        {
+            if (cancel_)
+            {
+                Log.LogMessage(MessageImportance.Low, "Exit on cancel signal");
+                return false;
+            }
+
+            Dictionary<string, PackagesConfig> projPackages = new Dictionary<string, PackagesConfig>();
+            Dictionary<PackagesConfig, ITaskItem> pkg2Items = new Dictionary<PackagesConfig, ITaskItem>();
+
+            string targetProject = DependecyProject.GetMetadata("FullPath");
+            if (string.IsNullOrWhiteSpace(targetProject) || !File.Exists(targetProject))
+            {
+                Log.LogError($"FullPath not found for target project {DependecyProject.ItemSpec}");
+                return false;
+            }
+
+            Log.LogMessage(MessageImportance.Low, $"First project is '{DependecyProject.ItemSpec}'. All projects count is {AllProjects.Length}");
+
+            string targetId = Path.GetFileNameWithoutExtension(targetProject);
+
+            // Accumulate package dependencies for all projects.
+            foreach (ITaskItem p in AllProjects)
+            {
+                string projPath = p.GetMetadata("FullPath");
+                Log.LogMessage(MessageImportance.Low, $"Inspecting '{projPath}'.");
+                if (string.IsNullOrWhiteSpace(projPath) || !File.Exists(projPath))
+                {
+                    Log.LogWarning($"FullPath not found for target project {p.ItemSpec}");
+                    continue;
+                }
+
+                // Project already handled?
+                string projId = Path.GetFileNameWithoutExtension(projPath);
+                if (targetProject.Equals(projPath) || projPackages.ContainsKey(projId))
+                {
+                    continue;
+                }
+
+                // package.config file exists?
+                string pkgCfg = Path.Combine(Path.GetDirectoryName(projPath), "packages.config");
+                if (!File.Exists(pkgCfg))
+                {
+                    Log.LogMessage(MessageImportance.Low, $"File not found '{pkgCfg}'.");
+                    continue;
+                }
+
+                Log.LogMessage(MessageImportance.Low, $"Parsing '{pkgCfg}'.");
+                PackagesConfig pc = new PackagesConfig(pkgCfg);
+                projPackages[projId] = pc;
+                pkg2Items[pc] = p;
+
+                if (cancel_)
+                {
+                    Log.LogMessage(MessageImportance.Low, "Exit on cancel signal");
+                    return false;
+                }
+            }
+
+            List<PackagesConfig> pcOrder = new List<PackagesConfig>();
+            foreach (PackagesConfig pc in projPackages.Values)
+            {
+                List<string> pcDep = pc.ResolveRecursiveDependencies(projPackages);
+                if (pcDep.Contains(targetId) && projPackages.ContainsKey(pc.Id) && !pcOrder.Contains(pc))
+                {
+                    Log.LogMessage(MessageImportance.Low, $"Build requires '{pc.Id}'.");
+                    pcOrder.Add(pc);
+                }
+
+                if (cancel_)
+                {
+                    Log.LogMessage(MessageImportance.Low, "Exit on cancel signal");
+                    return false;
+                }
+            }
+
+            // Resolve projects build order.
+            if (!SetBuildOrder(pcOrder))
+            {
+                return false;
+            }
+
+            List<TaskItem> orderded = new List<TaskItem>();
+            orderded.Add(new TaskItem(DependecyProject));
+            foreach (PackagesConfig pc in pcOrder)
+            {
+                Log.LogMessage($"Build order: {pc.Id}");
+                orderded.Add(new TaskItem(pkg2Items[pc]));
+            }
+
+            DependantProjectsBuildOrdered = orderded.ToArray();
+            return !cancel_;
+        }
+
+        private bool SetBuildOrder(List<PackagesConfig> pcOrder)
+        {
+            bool changed;
+            int changeCount = 0;
+            int maxChanges = pcOrder.Count * pcOrder.Count;
+
+            Log.LogMessage(MessageImportance.Low, "Resolving Build order...");
+
+            do
+            {
+                if (cancel_)
+                {
+                    Log.LogMessage(MessageImportance.Low, "Exit on cancel signal");
+                    return false;
+                }
+
+                if (changeCount > maxChanges)
+                {
+                    Log.LogError("ParseProjects", "Project dependency dead-lock. There's a circular dependency in projects.");
+                    return false;
+                }
+                changed = false;
+
+                for (int i = 0; i < pcOrder.Count; ++i)
+                {
+                    PackagesConfig proj1 = pcOrder[i];
+                    for (int j = i + 1; j < pcOrder.Count; ++j)
+                    {
+                        PackagesConfig proj2 = pcOrder[j];
+                        if (proj1.ResolveRecursiveDependencies(null).Contains(proj2.Id, StringComparer.OrdinalIgnoreCase))
+                        {
+                            Log.LogMessage(MessageImportance.Low, $"'{proj1.Id}' depends on '{proj2.Id}'");
+
+                            changed = true;
+                            pcOrder.RemoveAt(j);
+                            pcOrder.Insert(i, proj2);
+                            break;
+                        }
+                    }
+
+                    if (changed)
+                    {
+                        ++changeCount;
+                        break;
+                    }
+                }
+            } while (changed);
+
+            return true;
+        }
+
+        void ICancelableTask.Cancel()
+        {
+            cancel_ = true;
+        }
+
+        [Required]
+        public ITaskItem DependecyProject { get; set; }
+
+        [Required]
+        public ITaskItem[] AllProjects { get; set; }
+
+        [Output]
+        public ITaskItem[] DependantProjectsBuildOrdered { get; private set; }
+    }
+}
